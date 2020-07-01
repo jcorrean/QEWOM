9aed30c209d4b56659256da88daa2c1412d4f00c
Juan C. Correa <juanc.correan@konradlorenz.edu.co>
2020-07-01 20:14:18 +0200

    Filtering Restaurants

---
+++ FilteringRestaurants.R
@@ -0,0 +1,4 @@
+DefinitiveSample <- filter(df, grepl("Arroz y Pasta al Wok", Name) |
+                               grepl("Bogota Food Company", Name) |
+                               grepl("Casa Del Sushi", Name))
+                           
