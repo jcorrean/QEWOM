fcca08085268c3f08c4eee6f12ed7f595fc38499
Juan C. Correa <juanc.correan@konradlorenz.edu.co>
2020-07-02 12:21:48 +0200

    A simple script for selecting restaurants with at least 200 comments

--- FilteringRestaurants.R
+++ FilteringRestaurants.R
@@ -1,4 +1,25 @@
-DefinitiveSample <- filter(df, grepl("Arroz y Pasta al Wok", Name) |
-                               grepl("Bogota Food Company", Name) |
-                               grepl("Casa Del Sushi", Name))
-                           
+DefinitiveSample <- filter(df,
+                           grepl("Arroz y Pasta al Wok", Name) |
+                           grepl("Bogota Food Company", Name) |
+                           grepl("Casa Del Sushi", Name) |
+                           grepl("Charlies Roastbeef", Name) |  
+                           grepl("China Tao", Name) |
+                           grepl("Colombia and Pizza", Name) |
+                           grepl("Comidas Rapidas", Name) |
+                           grepl("Donde Beto", Name) |
+                           grepl("Donde Lucho", Name) |
+                           grepl("Donde Pele", Name) |
+                           grepl("El Señor del Pollo", Name) |
+                           grepl("Frisby", Name) |
+                           grepl("Hamburguesas Santa Fe", Name) |
+                           grepl("Hot Delivery Principal", Name) |
+                           grepl("Juancho", Name) |
+                           grepl("JYS", Name) |
+                           grepl("KFC", Name) |
+                           grepl("La Sazón", Name) |
+                           grepl("Manyares", Name) |
+                           grepl("Napoli", Name) |
+                           grepl("Perros", Name) |
+                           grepl("Pizza del Barrio", Name) |
+                           grepl("Presto", Name) |
+                           grepl("Sr Wok", Name))
