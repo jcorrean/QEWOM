2e7da2e19dbcc279c4d75ccc195f2037fda7c104
Juan C. Correa <juanc.correan@konradlorenz.edu.co>
2020-07-06 01:53:37 +0200

    Update QEWOM.R

--- QEWOM.R
+++ QEWOM.R
@@ -22,31 +22,38 @@ SelectedRestaurants <- filter(Restaurants, Freq > 200)
 # share the same commercial brand (e.g., KFC) and 
 # have different branch or point-of-sale.
 
-DefinitiveSample <- filter(
-  df, grepl("Arroz y Pasta al Wok", Name) |
-      grepl("Bogota Food Company", Name) |
-      grepl("Casa Del Sushi", Name) |
-      grepl("Charlies Roastbeef", Name) |  
-      grepl("China Tao", Name) |
-      grepl("Colombia and Pizza", Name) |
-      grepl("Comidas Rapidas", Name) |
-      grepl("Donde Beto", Name) |
-      grepl("Donde Lucho", Name) |
-      grepl("Donde Pele", Name) |
-      grepl("El Señor del Pollo", Name) |
-      grepl("Frisby", Name) |
-      grepl("Hamburguesas Santa Fe", Name) |
-      grepl("Hot Delivery Principal", Name) |
-      grepl("Juancho", Name) |
-      grepl("JYS", Name) |
-      grepl("KFC", Name) |
-      grepl("La Sazón", Name) |
-      grepl("Manyares", Name) |
-      grepl("Napoli", Name) |
-      grepl("Perros", Name) |
-      grepl("Pizza del Barrio", Name) |
-      grepl("Presto", Name) |
-      grepl("Sr Wok", Name))
+ToyDB <- filter(
+    df, grepl("Arroz y Pasta al Wok", Name) | 
+      grepl("Food Company", Name))
+
+
+#DefinitiveSample <- filter(
+#  df, grepl("Arroz y Pasta al Wok", Name) |
+#      grepl("Bogota Food Company", Name) |
+#      grepl("Casa Del Sushi", Name) |
+#      grepl("Charlies Roastbeef", Name) |  
+#      grepl("China Tao", Name) |
+#      grepl("Colombia and Pizza", Name) |
+#      grepl("Comidas Rapidas", Name) |
+#      grepl("Donde Beto", Name) |
+#      grepl("Donde Lucho", Name) |
+#      grepl("Donde Pele", Name) |
+#      grepl("El Señor del Pollo", Name) |
+#      grepl("Frisby", Name) |
+#      grepl("Hamburguesas Santa Fe", Name) |
+#      grepl("Hot Delivery Principal", Name) |
+#      grepl("Juancho", Name) |
+#      grepl("JYS", Name) |
+#      grepl("KFC", Name) |
+#      grepl("La Sazón", Name) |
+#      grepl("Manyares", Name) |
+#      grepl("Napoli", Name) |
+#      grepl("Perros", Name) |
+#      grepl("Pizza del Barrio", Name) |
+#      grepl("Presto", Name) |
+#      grepl("Sr Wok", Name))
+
+
 
 DefinitiveSample <- DefinitiveSample[c(1,8:11)]
 rm(list=setdiff(ls(), "DefinitiveSample"))
